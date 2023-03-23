c46ff7cd53f68637ed98c9a3aad06c6dd3b0235b
teem001 <107809346+teem001@users.noreply.github.com>
2023-03-23 22:33:48 +0100

    reload context

--- src/test/java/com/example/mybookstor/MyBookStorApplicationTests.java
+++ src/test/java/com/example/mybookstor/MyBookStorApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyBookStorApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
